5580d01f81fd201b26433798865f06c396a7d9ac
Aaron Miller <me@aaron-miller.me>
2015-07-08 10:56:52 -0400

    maintain back compat w/1.0.0 interface; version to 1.1.0

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-module.exports = {
+var obj = {
+  // issue #2 and going forward
   Encoder: require('lib/same'),
   Writer: require('lib/writer'),
   Values: require('lib/fixtures/same')
 };
+
+// back compat for all my no users :)
+obj.SAME = obj.Encoder;
+
+module.exports = obj;
