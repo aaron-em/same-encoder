5c093df1c2e35188ac5f488c8827692a9511677c
Aaron Miller <me@aaron-miller.me>
2015-12-05 06:18:54 -0500

    add jshint to gruntfile (closes #8)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ var grunt = require('grunt');
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-mocha-istanbul');
-  
+
   grunt.initConfig({
     shell: {
       serverDemo: {
@@ -16,6 +16,10 @@ module.exports = function(grunt) {
 
       testUnit: {
         command: 'node_modules/.bin/mocha test/unit/*.spec.js test/unit/**/*.spec.js'
+      },
+
+      jshint: {
+        command: 'find lib test -not -path \'test/reports/*\' -name \'*.js\' | xargs jshint'
       },
 
       browserify: {
@@ -36,14 +40,19 @@ module.exports = function(grunt) {
 
   grunt.registerTask('browserify', 'Bundle the library for browsers',
                      ['shell:browserify']);
-  
+
   grunt.registerTask('demo:client', 'Run the client-side demo server',
                      ['browserify', 'shell:clientDemo']);
   grunt.registerTask('demo:server', 'Run the server-side demo',
                      ['shell:serverDemo']);
 
+  grunt.registerTask('jshint', 'Run jshint check',
+                     ['shell:jshint']);
   grunt.registerTask('test', 'Run unit tests',
                      ['shell:testUnit']);
   grunt.registerTask('cover', 'Run unit tests + coverage report',
                      ['mocha_istanbul:coverage']);
+  grunt.registerTask('validate', 'Run all checks and generate coverage',
+                     ['shell:jshint',
+                      'mocha_istanbul:coverage']);
 };
